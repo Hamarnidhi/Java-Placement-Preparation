182d2945cf5c625409396b10c9fd982844289b94
Hamarnidhi <hamarnidhi@gmail.com>
2026-07-07 09:24:21 +0530

    Solved 02-LargestElement

---
+++ 01-Arrays/02-LargestElement.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] arr = new int[5];
+        for(int i=0;i<5;i++)
+        {
+            arr[i] = sc.nextInt();
+        }
+        int max = arr[0];
+        for(int i=0;i<5;i++)
+        {
+            if(arr[i] > max)
+            {
+                max = arr[i];
+            }
+        }
+        System.out.print(max);
+        sc.close();
+    }
+}
