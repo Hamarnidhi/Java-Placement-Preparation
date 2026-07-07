6366e0b39a8932e776ef32cba41b09edca7d3db6
Hamarnidhi <hamarnidhi@gmail.com>
2026-07-07 10:18:45 +0530

    Solved 01-SmallestElement

--- 01-Arrays/01-SmallestElement.java
+++ 01-Arrays/01-SmallestElement.java
@@ -1,10 +1,13 @@
-//import java.util.Scanner;
+import java.util.Scanner;
 class SmallestElement {
     public static void main(String[] args) {
-        //Scanner sc = new Scanner(System.in);
-        int[] arr = {121312,31243546,32434,61312,5};
+        Scanner sc = new Scanner(System.in);
+        int[] arr = new int[5];
+        for(int i=0;i<5;i++)
+        {
+            arr[i] = sc.nextInt();
+        }
         int min=Integer.MAX_VALUE;
-        // System.out.println(min);
         for(int i:arr){
             min=Math.min(i,min);
         }
